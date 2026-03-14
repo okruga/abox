0565ca9236b14a03dde98e699bae6518f0a76318
Denys Vasyliev <den.vasyliev@gmail.com>
2026-03-14 20:02:12 +0000

    Use kubernetes provider with config_path for RSIP+ResourceSet bootstrap

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -16,6 +16,7 @@ terraform {
     }
   }
 }
+
 provider "kind" {}
 
 provider "helm" {
@@ -28,8 +29,5 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = kind_cluster.this.endpoint
-  client_certificate     = kind_cluster.this.client_certificate
-  client_key             = kind_cluster.this.client_key
-  cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+  config_path = "~/.kube/config"
 }
