16c5595510e7290862577cf251d21f8a48566354
Denys Vasyliev <den.vasyliev@gmail.com>
2026-03-14 20:14:11 +0000

    Fix kubernetes provider to use kind_cluster credentials instead of config_path

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -29,5 +29,8 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = kind_cluster.this.endpoint
+  client_certificate     = kind_cluster.this.client_certificate
+  client_key             = kind_cluster.this.client_key
+  cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
 }
