baecb26340e2a41642bd848cfb5253db861d95c7
Denys Vasyliev <den.vasyliev@gmail.com>
2026-03-14 19:16:45 +0000

    Move flux_config helm release into flux.tf, remove flux-config.tf

--- bootstrap/flux.tf
+++ bootstrap/flux.tf
@@ -28,3 +28,16 @@ resource "helm_release" "flux_instance" {
     value = "=2.x"
   }
 }
+
+# ==========================================
+# Bootstrap Flux config via OCI artifact
+# ==========================================
+resource "helm_release" "flux_config" {
+  depends_on = [helm_release.flux_instance]
+
+  name       = "flux-config"
+  namespace  = "flux-system"
+  repository = var.oci_registry
+  chart      = "flux-config"
+  version    = "0.1.0"
+}
